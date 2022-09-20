1adaf4100c7182c279d94681678c5ce975ba3d1c
Wiwatcharachai <107343598+Wiwatcharachai@users.noreply.github.com>
2022-09-20 23:13:25 +0700

    HH:modules-by-min

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { ChakraProvider, Button,Spinner, Input} from '@chakra-ui/react';
+// import { ChakraProvider } from '@chakra-ui/react';
+import DisplayJohn,{JohnProfile} from './component/DisplayJohn';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChakraProvider>
+      
+     <Button color = "red" size = "ig">mybutton</Button>
+     <Button color = "yellow" size = "ig">mybutton</Button>
+     <Button color = "green" size = "ig">mybutton</Button>
+
+     <Spinner color = "pink"></Spinner>
+     <Spinner color = "black"></Spinner>
+     <Spinner color = "gray"></Spinner>
+    
+     <JohnProfile/>
+      </ChakraProvider>
     </div>
   );
 }
+
+
+
+
+
 
 export default App;
